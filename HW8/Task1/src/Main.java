844764b0e764774913ecc6fba5c45243f474c7ca
x1zet <155456277+x1zet@users.noreply.github.com>
2024-11-07 13:51:30 +0300

    ready

--- HW8/Task1/src/Main.java
+++ HW8/Task1/src/Main.java
@@ -3,13 +3,36 @@ public class Main {
         //#1 Создать массив из 10 целых чисел. Значения могут быть любыми
         //todo
 
+        int[] array = {1,2,3,4,5,6,7,8,9,10};
+
         //#2 Распечатать все значения массива начиная с 0 до последнего индексов.
         //todo
+
+//        for (int i = 0; i < array.length; i++) {
+//            System.out.println(array[i]);
+//        }
 
         //#3 Распечатать все значения массива с конца (начиная с последнего до 0 индексов).
         //todo
 
+//        for (int i = array.length - 1; i >= 0 ; i--) {
+//            System.out.println(array[i]);
+//        }
+
         //#4 *** Задание особой сложности. Развернуть массив(см конец урока).
         //todo
+
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = temp;
+        }
+
+        System.out.println("Развернутый массив:");
+
+        for (int i  = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+        }
+
     }
 }
--- HW8/Task2/src/Main.java
+++ HW8/Task2/src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         //todo код программы писать тут
+
+        int[] people = new int[10];
+        double summ = 0;
+        int countInRange  = 0;
+        for (int i = 0; i < people.length; i++) {
+            people[i] = new Random().nextInt(61) + 40;
+        }
+        for (int i = 0; i < people.length; i++) {
+            summ += people[i];
+        }
+
+        for (int i:people){
+            countInRange++;
+        }
+
+        System.out.println("Средний вес всех людей: " + summ / people.length + " кг");
+        System.out.println("Количество людей у которых вес находится в промежутке от 60 до 80 кг включительно: " + countInRange);
     }
 }
